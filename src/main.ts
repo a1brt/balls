9e640daab10b09c0ba4c2262f1fba448e65ff8e9
Sahak <s.iskanian33@gmail.com>
2024-05-20 10:14:35 +0400

    working buggy mess

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { Circle } from "./Circle";
 import "./style.css";
 
 const canvas = <HTMLCanvasElement>document.getElementById("canvas");
-let ball: Circle;
+const circles: Circle[] = [];
 let ctx: CanvasRenderingContext2D;
 
 window.onload = () => {
@@ -13,35 +13,48 @@ window.onload = () => {
     return;
   }
   ctx = c;
-  ball = new Circle(ctx,100,100);
 };
 
 function draw() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
- 
-  ball.draw();
-  ball.x += ball.vx;
-  ball.y += ball.vy;
-  ball.vy *= 0.99;
-  ball.vy += 0.25;
-
-  if (
-    ball.y + ball.vy > canvas.height - ball.radius ||
-    ball.y + ball.vy < ball.radius
-  ) {
-    ball.vy = -ball.vy;
-  }
-  if (
-    ball.x + ball.vx > canvas.width - ball.radius ||
-    ball.x + ball.vx < ball.radius
-  ) {
-    ball.vx = -ball.vx;
-  }
-
   
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for(let circle of circles){
+    console.log(circle);
+    
+
+    circle.draw();
+    circle.x += circle.vx;
+    circle.y += circle.vy;
+    circle.vy *= 0.99;
+    circle.vy += 0.25;
+  
+    if (
+      circle.y + circle.vy > canvas.height - circle.radius ||
+      circle.y + circle.vy < circle.radius
+    ) {
+      circle.vy = -circle.vy;
+    }
+    if (
+      circle.x + circle.vx > canvas.width - circle.radius ||
+      circle.x + circle.vx < circle.radius
+    ) {
+      circle.vx = -circle.vx;
+    }
+  
+  }
   window.requestAnimationFrame(draw);
 }
 
-canvas.addEventListener("mouseover", (e) => {
-   window.requestAnimationFrame(draw);
+document.getElementById("start")?.addEventListener("click", function () {
+  this.style.display = "none";
+  start()
 });
+
+function start() {
+  canvas.addEventListener("mousedown", function (e) {
+    circles.push(new Circle(ctx, e.clientX, e.clientY));
+    console.log(circles);
+  });
+
+  window.requestAnimationFrame(draw);
+}
